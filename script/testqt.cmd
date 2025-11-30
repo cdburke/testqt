a6b2016f00d862484db4067acf518b04ce284247
Chris Burke <cdburke@gmail.com>
2025-11-30 15:50:19 +0800

    ip

---
+++ script/testqt.cmd
@@ -0,0 +1,9 @@
+
+echo.
+echo %GITHUB_WORKSPACE%
+echo.
+dir %GITHUB_WORKSPACE%\Qt\6.10.1
+echo.
+dir %GITHUB_WORKSPACE%\Qt\6.10.1\msvc2022_arm64\bin
+echo.
+dir %GITHUB_WORKSPACE%\Qt\6.10.1\msvc2022_arm64\bin\host-qmake.bat
